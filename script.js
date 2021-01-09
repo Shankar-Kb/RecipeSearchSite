b14184f54c0a4bde960893866d9abb2781c11a75
Shankar-Kb <shankar.kb95@gmail.com>
2021-01-09 15:20:20 +0530

    Finished card design

--- script.js
+++ script.js
@@ -19,25 +19,56 @@ getRecipes();
 function createCards(data){
     let cardContainer = document.getElementById('cardContainer');
     
-    data.forEach(elem => {
+    data.forEach( (elem,index) => {
         var card = createHtmlElement('div', 'card');
         card.style = 'width: 18rem';
         cardContainer.append(card);
         
+        var cardBody = createHtmlElement('div', 'card-body');
+
         var cardHeader = createHtmlElement('div', 'card-header text-center');
         cardHeader.innerHTML = `<a class ='card-source' href='${elem.recipe.url}'>${elem.recipe.label}</a>`;
 
         var cardImg = createHtmlElement('img', 'card-image');
         cardImg.src = elem.recipe.image;
         cardImg.alt = elem.recipe.label;
+        
+        var cardText = createHtmlElement('p', 'card-text');
+        
+        cardText.innerHTML = `<span><span class='bold'>Health Labels:</span>${getLabels(elem.recipe.healthLabels)}</span>
+        <span><span class='bold'>Calories:</span> ${elem.recipe.calories.toFixed(0)} Cal</span>
+        <span><span class='bold'>Vitamins:</span><span class='vitamins-list'>${getVitamins(elem.recipe.digest)}</span>`;
 
+        var cardIngredients = createHtmlElement('span', 'btn');
+        cardIngredients.setAttribute('data-toggle', 'collapse');
+        cardIngredients.setAttribute('data-target', `#I${index}`);
+        cardIngredients.innerHTML = `<span class='ingredients-title'><i class="fas fa-utensils fa-1x"></i> Ingredient List</span>`;
 
-
-        card.append(cardHeader, cardImg);
+        var ingredientsContent = createHtmlElement('div', 'collapse ingredients-content', `I${index}`);
+        ingredientsContent.innerHTML = `${elem.recipe.ingredientLines}`;
+        
+        cardBody.append(cardText, cardIngredients, ingredientsContent);
+        card.append(cardHeader, cardImg, cardBody);
+        
 
 
     })
     
+}
+
+function getLabels(data){
+  var labelArray = data.map(elem => { return ' '+elem});
+  return labelArray;
+}
+function getVitamins(data){
+  var vitaminsOnly = data.filter( elem => { return elem.label.includes('Vitamin') })
+      .sort( (a,b) => { return b.total - a.total} );
+  
+  var vitaminsArray = [];
+  for(var i = 0; i<vitaminsOnly.length-2; i++){
+      vitaminsArray.push(' '+vitaminsOnly[i].label+'('+vitaminsOnly[i].total.toFixed(0)+vitaminsOnly[i].unit+')');
+  }
+  return vitaminsArray;
 }
 
 /* <div class="card" style="width: 18rem;">
